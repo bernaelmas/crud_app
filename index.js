4dbe4b50bc21f3e050d9df4daa4553ff983529d1
Berna Elmas <bernaelms1368@gmail.com>
2024-07-02 17:08:53 +0300

    Server Port Changed

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./models/product.model.js");
+// const Product = require("./models/product.model.js");
+const cors = require("cors");
 const productRoute = require("./routes/product.route.js");
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
 
 // routes
 app.use("/api/products", productRoute);
@@ -21,8 +24,8 @@ mongoose
   )
   .then(() => {
     console.log("Connected to Database!");
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
+    app.listen(5173, () => {
+      console.log("Server is running on port 5173");
     });
   })
   .catch(() => {
